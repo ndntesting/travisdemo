c34bbe6ecabb96ae043d7267b9e275578483d19d
ndntesting <ndntesting@outlook.com>
2018-05-31 22:19:29 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+dotnet restore src/travisdemo/*.csproj && dotnet build src/travisdemo/*.csproj
---
+++ src/travisdemo/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace travisdemo
+{
+    public class Class1
+    {
+    }
+}
